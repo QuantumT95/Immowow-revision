7a8f9b0653b77bffe17c9f93159e39530ab9b752
QuantumT95 <tran.charlie@ymail.com>
2018-12-05 23:18:48 -0800

    updated contact page with firebase to collect customer info + requests/messages

--- js/contact.js
+++ js/contact.js
@@ -25,6 +25,19 @@ $(document).ready(function(){
       }
   }
 
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAs8vJ5PmfEl4MxMjb7WndUoHSQ00W99vQ",
+    authDomain: "immowow-edea1.firebaseapp.com",
+    databaseURL: "https://immowow-edea1.firebaseio.com",
+    projectId: "immowow-edea1",
+    storageBucket: "immowow-edea1.appspot.com",
+    messagingSenderId: "915673792281"
+  };
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
   $('#btnSubmit').bind("click",function(event){
     // initialize variables
     var nameVal = $("#name-input").val();
@@ -45,6 +58,7 @@ $(document).ready(function(){
     };
 
     event.preventDefault();
+    database.ref().push(contactObj);
     console.log(contactObj);
   });
   // you can use the above or the one shown below
